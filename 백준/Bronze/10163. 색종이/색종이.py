d1908920f4f239e169f6b95e54fd66b323c7e344
KwonYuJu <dpwls0875@gmail.com>
2024-09-12 11:42:43 +0900

    [Bronze I] Title: 색종이, Time: 632 ms, Memory: 38300 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -5,12 +5,11 @@ arr = [[0]*1001 for _ in range(1001)]
 for n in range(1, N+1):
     sj, si, w, h = map(int, input().split())
     for i in range(si, si+h):
-        for j in range(sj, sj+w):
-            arr[i][j] = n
+        arr[i][sj:sj+w] = [n]*w
 
-# 색종이 번호 count (면적 구하기)
-for n in range(1, N+1):
-    cnt = 0
-    for lst in arr:
-        cnt += lst.count(n)
-    print(cnt)
+# cnts 빈도수 배열 사용해서 색종이 번호 count (arr한 번만 순회)
+cnts = [0]*(N+1)
+for lst in arr:
+    for i in lst:     
+        cnts[i] += 1  
+print(*cnts[1:], sep='\n')
